9c8f2b37a009f5ee9660711dca64db215760f51b
Shahbaz <m.azam.shahbaz@gmail.com>
2017-04-01 17:38:00 +0500

    First commit

---
+++ src/main/java/com/example/MyRestController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+	
+	 @RequestMapping(value = "/test/", method = RequestMethod.GET)
+	    public void listAllUsers() {
+	        
+	    }
+
+}
